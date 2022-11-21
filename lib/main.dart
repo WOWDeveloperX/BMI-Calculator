086805155178cd8b891bf2ae65367f7e17604c0b
«Igor Ignatkin» <IGN-Dart@yandex.ru>
2022-11-21 20:37:44 +0300

    theme screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,26 @@ import 'package:flutter/material.dart';
 void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
+  const BMICalculator({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: InputPage(),
+      theme: ThemeData.dark().copyWith(
+        //
+        colorScheme: const ColorScheme.light(
+          primary: Color(0xff0A0E21),
+        ),
+        scaffoldBackgroundColor: const Color(0xff0A0E21),
+      ),
+      home: const InputPage(),
     );
   }
 }
 
 class InputPage extends StatefulWidget {
+  const InputPage({super.key});
+
   @override
   _InputPageState createState() => _InputPageState();
 }
@@ -21,14 +32,20 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
+        title: const Text('BMI CALCULATOR'),
       ),
-      body: Center(
+      body: const Center(
         child: Text('Body Text'),
       ),
-      floatingActionButton: const FloatingActionButton(
-        onPressed: null,
-        child: Icon(Icons.add),
+      floatingActionButton: Theme(
+        data: ThemeData(
+          colorScheme:
+              ColorScheme.fromSwatch().copyWith(secondary: Colors.amber),
+        ),
+        child: const FloatingActionButton(
+          onPressed: null,
+          child: Icon(Icons.add),
+        ),
       ),
     );
   }
